966df16b0e68bb199b9985fda22cf2e2e680f231
danishsaifi810 <danishsaifi90786@gmail.com>
2026-01-06 12:45:52 +0530

    completed  revision js

--- revision.js
+++ revision.js
@@ -1,24 +1,87 @@
+/*This Plus Example*/
 let a = 10;
 let b = 20;
 
 console.log(a+b);
 
+/*This Minus Example*/
 let c = 10;
 let d = 20;
 
 console.log(c-d);
 
+/*This Multiply Example*/
 let e = 10;
 let f = 20;
 
 console.log(e*f);
 
+/*This Divide Example*/
 let g = 10;
 let h = 20;
 
 console.log(g/h);
 
+/*This Modulus Example*/
 let i = 10;
 let j = 20;
 
 console.log(i%j);
+
+/*This IndexOf Example*/
+let k = ("My Name Is Danish");
+let l = k.indexOf("D");
+
+console.log(l);
+
+/*This Duplicate Sub String Example*/
+let m = ("my name is danish my favorite color is blue");
+let n = m.indexOf("my", 3);
+
+console.log(n);
+
+/*This String Interpolation Example*/
+let o = ("Name Is Danish");
+let p = `My, ${o}!`;
+
+console.log(p);
+
+/*This Char Code At Example*/
+let q = "a";
+console.log (q.charCodeAt(0));
+
+/*This From Char Code Example*/
+let r = "97";
+console.log (String.fromCharCode(r));
+
+/*This Includes Example*/
+let s = "xyz";
+let t = s.includes("xyz");
+console.log(t);
+
+/*slice Example*/
+let u = ("Hello How Are You");
+let v = u.indexOf("H", 3);
+
+console.log(v);
+
+let w = u.slice(6, 9);
+console.log(w);
+
+/*trim Example*/
+let x = ("      hello")
+let y = x.trim();
+
+console.log(y);
+
+/*toLowerCase Example*/
+let z = ("hello my name is danis");
+let upp = z.toUpperCase();
+
+console.log(upp);
+
+/*toUpperCase Example*/
+let aa = ("HELLO MY NAME IS DANISH");
+let low = aa.toLowerCase();
+
+console.log(low);
